fa2cfc4096b9b2a66bae3c3e0303459c5602c584
BuenoBueno <43422079+BuenoBueno@users.noreply.github.com>
2018-10-10 19:19:53 -0300

    v.1.0
    Exercicio1

---
+++ algoritmo3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritmo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio1");
+            Console.WriteLine();
+
+            Console.Write("Digite o ano de nascimento: ");
+            int anonasc  = Convert.ToInt32(Console.ReadLine());
+            int idade = 2018 - anonasc;
+            Console.WriteLine("Idade: " + idade);
+
+            Console.WriteLine();
+
+            if (idade >18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else if(idade<18)
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            else if(idade==18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            Console.ReadKey();      
+        }
+    }
+}
